3b3544fe76942c677a3c3167abebfeea5d23c076
EduardoHdez <jhernandezg1403@gmail.com>
2026-02-05 17:45:56 -0600

    Añade javascript a proyecto general

--- js/products.js
+++ js/products.js
@@ -57,27 +57,26 @@ function desplegarProductos() {
 
     for (let i = 3; i < allProducts.length; i++) {
         productosHTML += `
-    <div class="col-md-4 ">
-                <div class="card h-100 product-card border-0 shadow-sm">
-                    <div class="product-image-wrapper position-relative">
-                        <img src="${allProducts[i].img}"
-                            class="card-img-top" alt="Zapatillas Running">
-                        <div class="product-image-overlay">
-                            <p class="mb-1 fw-bold">${allProducts[i].name}</p>
-                            <p class="mb-1 small">${allProducts[i].description}</p>
-                        </div>
-                        <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
-                    </div>
-
-                    <div class="card-body text-center">
-                        <h5 class="card-title fw-bold">${allProducts[i].name}</h5>
-                        <p class="card-text text-muted">${allProducts[i].category}</p>
-                        <h4 class="text-primary fw-bold">$${allProducts[i].price}</h4>
-                        <button class="btn btn-outline-dark w-100 mt-2 add-to-cart" onclick="addToCart(this)">Añadir al
-                            Carrito</button>
-                    </div>
-                </div>
+    <div class="col-md-4">
+        <div class="card h-100 product-card border-0 shadow-sm">
+          <div class="product-image-wrapper position-relative">
+            <img src="${allProducts[i].img}" 
+                 class="card-img-top" 
+                 alt="Zapatillas Running">
+            <div class="product-image-overlay">
+              <p class="mb-1 fw-bold">${allProducts[i].name}</p>
+              <p class="mb-1 small">${allProducts[i].description}</p>
             </div>
+            <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title fw-bold">${allProducts[i].name}</h5>
+            <p class="card-text text-muted">${allProducts[i].category}</p>
+            <h4 class="text-primary fw-bold">$${allProducts[i].price}</h4>
+            <button class="btn btn-outline-dark w-100 mt-2 add-to-cart" onclick="addToCart(this)">Añadir al Carrito</button>
+          </div>
+        </div>
+      </div>
     `;
 
     }
@@ -91,27 +90,26 @@ function cargarProductosIniciales() {
 
     for (let i = 0; i < 3; i++) {
         productosHTMLInicial += `
-    <div class="col-md-4 ">
-                <div class="card h-100 product-card border-0 shadow-sm">
-                    <div class="product-image-wrapper position-relative">
-                        <img src="${allProducts[i].img}"
-                            class="card-img-top" alt="Zapatillas Running">
-                        <div class="product-image-overlay">
-                            <p class="mb-1 fw-bold">${allProducts[i].name}</p>
-                            <p class="mb-1 small">${allProducts[i].description}</p>
-                        </div>
-                        <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
-                    </div>
-
-                    <div class="card-body text-center">
-                        <h5 class="card-title fw-bold">${allProducts[i].name}</h5>
-                        <p class="card-text text-muted">${allProducts[i].category}</p>
-                        <h4 class="text-primary fw-bold">$${allProducts[i].price}</h4>
-                        <button class="btn btn-outline-dark w-100 mt-2 add-to-cart" onclick="addToCart(this)">Añadir al
-                            Carrito</button>
-                    </div>
-                </div>
+    <div class="col-md-4">
+        <div class="card h-100 product-card border-0 shadow-sm">
+          <div class="product-image-wrapper position-relative">
+            <img src="${allProducts[i].img}" 
+                 class="card-img-top" 
+                 alt="Zapatillas Running">
+            <div class="product-image-overlay">
+              <p class="mb-1 fw-bold">${allProducts[i].name}</p>
+              <p class="mb-1 small">${allProducts[i].description}</p>
             </div>
+            <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title fw-bold">${allProducts[i].name}</h5>
+            <p class="card-text text-muted">${allProducts[i].category}</p>
+            <h4 class="text-primary fw-bold">$${allProducts[i].price}</h4>
+            <button class="btn btn-outline-dark w-100 mt-2 add-to-cart" onclick="addToCart(this)">Añadir al Carrito</button>
+          </div>
+        </div>
+      </div>
     `;
     }
 
