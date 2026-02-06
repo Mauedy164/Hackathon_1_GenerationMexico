8be49d8ac09df1b98469011da52c43f10da689c1
ignaciozg <ignacio.zgxd27@gmail.com>
2026-02-05 18:35:25 -0800

    Update

---
+++ js/carrusel.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+        // 1. Seleccionamos el contenedor y los items
+        const container = document.querySelector('.hero-carrusel');
+        const slides = document.querySelectorAll('.hero-item');
+        
+        // 2. Configuración
+        const intervalTime = 4000; // Tiempo en milisegundos (5000ms = 5 segundos)
+        let currentSlide = 0;
+        let slideInterval;
+
+        // 3. Función que realiza el movimiento
+        const nextSlide = () => {
+            currentSlide++;
+            
+            // Si llegamos al final, volvemos al principio (slide 0)
+            if (currentSlide >= slides.length) {
+                currentSlide = 0;
+            }
+
+            // Calculamos cuánto hay que desplazar (Ancho del contenedor * número de slide)
+            const width = container.offsetWidth;
+            container.scrollTo({
+                left: width * currentSlide,
+                behavior: 'smooth' // Desplazamiento suave
+            });
+        };
+
+        // 4. Iniciar el ciclo automático
+        slideInterval = setInterval(nextSlide, intervalTime);
+
+        // 5. (Opcional) Pausar si el usuario pasa el mouse por encima
+        // Esto mejora la experiencia para que el usuario pueda leer sin que se mueva
+        container.addEventListener('mouseenter', () => {
+            clearInterval(slideInterval);
+        });
+
+        container.addEventListener('mouseleave', () => {
+            slideInterval = setInterval(nextSlide, intervalTime);
+        });
+    });
---
+++ js/products.js
@@ -0,0 +1,127 @@
+// botón ver más
+
+
+const API_URL = './data/products.json'
+
+const btn_VerMas = document.getElementById("btn_VerMas")
+const product_container = document.getElementById("product_container")
+const btn_VerMenos = document.getElementById("btn_VerMenos")
+
+
+btn_VerMas.addEventListener('click', desplegarProductos)
+btn_VerMenos.addEventListener('click', ocultarProductos)
+
+const productosIniciales = product_container.innerHTML
+
+allProducts = []
+
+//Fetch
+
+function fetchProductsList() {
+    const options = { 'method': 'GET' }
+    fetch(API_URL, options)
+        .then((response) => {
+            response.json().then((products) => {
+                for (let i = 0; i < products.length; i++) {
+                    console.log(products[i]);
+                }
+                allProducts = products
+
+
+                localStorage.setItem('productos', JSON.stringify(products))
+                cargarProductosIniciales()
+            })
+        })
+        .catch((err) => { console.log(err.message); })
+}
+
+function loadProductsFromStorage() {
+    const productosStorage = localStorage.getItem('productos');
+
+    if (productosStorage) {
+        allProducts = JSON.parse(productosStorage);
+        console.log("Productos cargados desde el localStorage");
+        cargarProductosIniciales()
+    } else {
+        console.log("Productos cargados desde fetch");
+        fetchProductsList();
+    }
+}
+
+
+function desplegarProductos() {
+    btn_VerMenos.style.display = 'block'
+    btn_VerMas.style.display = "none"
+
+    let productosHTML = ''
+
+    for (let i = 3; i < allProducts.length; i++) {
+        productosHTML += `
+    <div class="col-md-4">
+        <div class="card h-100 product-card border-0 shadow-sm bg-dark text-white">
+          <div class="product-image-wrapper position-relative">
+            <img src="${allProducts[i].img}" 
+                 class="card-img-top" 
+                 alt="Zapatillas Running">
+            <div class="product-image-overlay">
+              <p class="mb-1 fw-bold">${allProducts[i].name}</p>
+              <p class="mb-1 small">${allProducts[i].description}</p>
+            </div>
+            <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title fw-bold">${allProducts[i].name}</h5>
+            <p class="card-text text-white-50">${allProducts[i].category}</p>
+            <h4 class="text-primary fw-bold text-white">$${allProducts[i].price}</h4>
+            <button class="btn btn-secondary w-100 mt-2 add-to-cart text-white" onclick="addToCart(this)">Añadir al Carrito</button>
+          </div>
+        </div>
+      </div>
+    `;
+
+    }
+
+    product_container.innerHTML += productosHTML
+
+}
+
+function cargarProductosIniciales() {
+    let productosHTMLInicial = ''
+
+    for (let i = 0; i < 3; i++) {
+        productosHTMLInicial += `
+    <div class="col-md-4">
+        <div class="card h-100 product-card border-0 shadow-sm bg-dark text-white">
+          <div class="product-image-wrapper position-relative">
+            <img src="${allProducts[i].img}" 
+                 class="card-img-top" 
+                 alt="Zapatillas Running">
+            <div class="product-image-overlay">
+              <p class="mb-1 fw-bold text-white">${allProducts[i].name}</p>
+              <p class="mb-1 small text-white">${allProducts[i].description}</p>
+            </div>
+            <span class="badge bg-danger position-absolute top-0 start-0 m-3">-20%</span>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title fw-bold text-white">${allProducts[i].name}</h5>
+            <p class="card-text  text-white-50">${allProducts[i].category}</p>
+            <h4 class="text-primary fw-bold text-white">$${allProducts[i].price}</h4>
+            <button class="btn btn-secondary w-100 mt-2 add-to-cart text-white" onclick="addToCart(this)">Añadir al Carrito</button>
+          </div>
+        </div>
+      </div>
+    `;
+    }
+
+    product_container.innerHTML = productosHTMLInicial
+}
+
+function ocultarProductos() {
+    btn_VerMenos.style.display = 'none'
+    btn_VerMas.style.display = "block"
+    cargarProductosIniciales()
+
+}
+
+
+loadProductsFromStorage()
